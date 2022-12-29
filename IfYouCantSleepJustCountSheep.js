1dd20f0f423508204d98f3610ddb0a7f10ee6d88
KirillMar <re4r@bk.ru>
2022-12-29 20:59:59 +0300

    ten

---
+++ IfYouCantSleepJustCountSheep.js
@@ -0,0 +1,11 @@
+var countSheep = function (num){
+  let count = '';
+  if (num === 0) {
+    return count;
+  } else {
+    for (let i = 1; i <= num; i++) {
+      count += i + ' sheep...';
+    }
+  }
+  return count;
+};
