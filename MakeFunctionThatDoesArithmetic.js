d8add2c741794616f2bbdc5d468c6d7aaa94dbdb
KirillMar <re4r@bk.ru>
2022-12-31 21:42:06 +0300

    twentyTwo

---
+++ MakeFunctionThatDoesArithmetic.js
@@ -0,0 +1,18 @@
+function arithmetic(a, b, operator){
+    switch (operator) {
+        case 'add':
+            return a + b;
+            break;
+        case 'subtract':
+            return a - b;
+            break;
+        case 'multiply':
+            return a * b;
+            break;
+        case 'divide':
+            return a / b;
+            break;            
+    }
+  };
+
+  console.log(arithmetic(1, 2, "add"));
