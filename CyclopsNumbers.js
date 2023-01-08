6ad3d5ace6f441607f59dabff7ccdb05861f4dc6
KirillMar <re4r@bk.ru>
2023-01-08 21:48:19 +0300

    fifty

---
+++ CyclopsNumbers.js
@@ -0,0 +1,15 @@
+function cyclops(n) {
+    const binary = n.toString(2);
+    let numberOfZero = 0;
+
+    for (let elem of binary) {
+        if (elem === '0') numberOfZero++;
+    }
+    
+    const sequenceLength = binary.length;
+    const middleElement = binary[Math.floor(sequenceLength / 2)];
+
+    return sequenceLength % 2 && middleElement === '0' && numberOfZero === 1 ? true : false;
+};
+
+console.log(cyclops());
