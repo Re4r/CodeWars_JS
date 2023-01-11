a7bb76c797201a83bdffbf7b9ff7686aa26fab5e
KirillMar <re4r@bk.ru>
2023-01-11 18:38:49 +0300

    sixtySeven

---
+++ DuplicateEncoder.js
@@ -0,0 +1,21 @@
+function duplicateEncode(word) {
+    const input = word.toLowerCase().split('');
+    const inputObj = new Map;
+    const output = [];
+
+    for (let item of input) {
+        if (inputObj[item] === undefined) {
+            inputObj[item] = 0;
+        } else {
+            inputObj[item] = 1;
+        }
+    }
+
+    for (let element of input) {
+        if (inputObj[element] === 0) output.push('(');
+        if (inputObj[element] === 1) output.push(')');
+    }
+    return output.join('');
+};
+
+console.log(duplicateEncode("recede"));
