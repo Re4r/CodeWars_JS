2188b1684905c18822aefe1ad0633c25e20053b1
KirillMar <re4r@bk.ru>
2023-01-11 13:23:55 +0300

    sixtyOne

--- StopgninnipSMysdroW.js
+++ StopgninnipSMysdroW.js
@@ -1,5 +1,13 @@
 function spinWords(str) {
+    const input = str.split(' ');
+    
+    for (let i = 0; i < input.length; i++) {
+        if (input[i].length >= 5) {
+            input[i] = input[i].split('').reverse().join('');
+        }
+    }
 
+    return input.join(' ');
 };
 
 console.log(spinWords("Hey fellow warriors"));
